503e253fa80aea045a90430cadb945a4faac5340
Alfu <Alfulinku@gmail.com>
2024-06-13 15:23:06 +0200

    Update EventWatcher.ts

--- src/EventWatcher.ts
+++ src/EventWatcher.ts
@@ -1,42 +1,19 @@
-import { ethers, Contract, Interface } from 'ethers';
-import WebSocket from 'ws';
+import { Contract, Interface, JsonRpcProvider } from 'ethers';
 import dotenv from 'dotenv';
 import { metamorphoAbi } from './abis/MetaMorphoAbi';
 import { EventQueue } from './EventQueue';
 dotenv.config();
 
-const WSS_URL: string | undefined = process.env.WSS_PROVIDER;
+const RPC_URL: string | undefined = process.env.RPC_URL;
 const METAMORPHO_ADDRESS: string | undefined = process.env.METAMORPHO_ADDRESS;
-
-let provider = new ethers.WebSocketProvider(createWebSocket());
-
-function createWebSocket() {
-  if (!WSS_URL) {
-    throw new Error('No WSS_URL found in env');
-  }
-  const ws = new WebSocket(WSS_URL);
-
-  ws.on('close', () => {
-    console.log('Disconnected. Reconnecting...');
-    setTimeout(() => {
-      provider = new ethers.WebSocketProvider(createWebSocket());
-      startListening();
-    }, 1000);
-  });
-
-  ws.on('error', (error) => {
-    console.log('WebSocket error: ', error);
-  });
-
-  return ws;
-}
 
 function startListening() {
   if (!METAMORPHO_ADDRESS) {
     throw new Error('No METAMORPHO_ADDRESS found in env');
   }
   console.log('Started the event listener');
-  const metamorphoContract = new Contract(METAMORPHO_ADDRESS, metamorphoAbi, provider);
+  const web3Provider = new JsonRpcProvider(RPC_URL, undefined, { staticNetwork: true, pollingInterval: 15_000 });
+  const metamorphoContract = new Contract(METAMORPHO_ADDRESS, metamorphoAbi, web3Provider);
 
   const iface = new Interface(metamorphoAbi);
 
